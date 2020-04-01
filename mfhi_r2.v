a3ccb17641f51ecfdf4bce5a6240040ec03805cd
16elp <44043175+16elp@users.noreply.github.com>
2020-04-01 13:50:48 -0400

    Update and rename mfhi_r2.txt to mfhi_r2.v

--- mfhi_r2.v
+++ mfhi_r2.v
@@ -1,35 +1,44 @@
 	
 //insruction: B9000000
+
+
 Reg_load1a: begin
 	Mdatain <= 32'h00001234; 
 	enableMDR <= 1; MDR_read<=3'd2;
 	#15 enableMDR <= 0;MDR_read<=3'd0;
 end
-Reg_load1b: begin    // initialize HI with the value 00001234
+
+Reg_load1b: begin  
 	MDRout <= 1; enableHI<=1;   
 	#15 MDRout <= 0; enableHI<= 0; 
 end
+
 Reg_load3a: begin
 	Mdatain <= 32'h00000000;   
 	enableMDR <= 1;MDR_read<=3'd2;
 	#15 enableMDR <= 0;MDR_read<=3'd0; 
 end
+
 Reg_load3b: begin
 	MDRout <= 1; enablePC<=1;	
 	#15 MDRout <= 0; enablePC<=0;
 end
+
 T0: begin 
 	PCout <= 1; enableMAR <= 1; IncPC<=1; enableZ<=1;
 	#15 PCout <= 0; enableMAR <= 0;  IncPC<=0; enableZ<=0;
 end
-T1: begin //Loads MDR from RAM output 
+
+T1: begin  
 	enableMDR <= 1; MDR_read<=3'd1; ZLowout<=1; enablePC<=1;   
 	#15 enableMDR <= 0; MDR_read<=3'd0; ZLowout<=0; enablePC<=0;
 end
+
 T2: begin
 	MDRout <= 1; IR_enable <= 1;
 	#15 MDRout <= 0; IR_enable <= 0;			
 end
+
 T3: begin
 	Gra<=1;R_enable<=1;HIout<=1;
 	#15 Gra<=0;R_enable<=0;HIout<=1;
