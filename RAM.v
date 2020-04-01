8333c58fb5d1761a98deeb9aa5098da166492267
16elp <44043175+16elp@users.noreply.github.com>
2020-03-31 21:56:31 -0400

    Update RAM.v

--- RAM.v
+++ RAM.v
@@ -1,45 +1,19 @@
-//Quartus II Verilog Template
-//Single port RAM with single read/write address 
 
-module RAM(
-	input [31:0] data,
-	input [7:0] addr,
-	input we, clk,
-	output [31:0] q
-);
+module RAM(input [31:0] data, input [7:0] address, input we, clk, output [31:0] q);
 
-	// Declare the RAM variable
 	reg [31:0] ram[511:0];
-
-	// Variable to hold the registered read address
-	reg [31:0] addr_reg;
-	
-	// Specify the initial contents.  You can also use the $readmemb
-	// system task to initialize the RAM variable from a text file.
-	// See the $readmemb template page for details.
+	reg [31:0] addressReg;
 	
 	initial
 	begin : INIT
 		$readmemh("init.mif", ram); 
 	end
-
 	
 	always @(posedge clk)
 	begin
-		// Write
 		if (we)
-			ram[addr] <= data;
-
-		addr_reg <= addr;
+			ram[address] <= data;
+		addressReg <= address;
 	end
-	
-	
-	assign q = ram[addr_reg];
-			// Continuous assignment implies read returns NEW data.
-			// This is the natural behavior of the TriMatrix memory
-			// blocks in Single Port mode.  
-
-//	always @ (clk)	
-//	 $monitor( "q=%h",ram[addr_reg]);
-
+	assign q = ram[addressReg];
 endmodule
