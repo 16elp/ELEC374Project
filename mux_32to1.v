e38a833b4963fe13440aeafef7248f8740cb860b
16elp <44043175+16elp@users.noreply.github.com>
2020-04-01 11:31:03 -0400

    Update mux_32to1.v

--- mux_32to1.v
+++ mux_32to1.v
@@ -1,65 +1,60 @@
-// File Name: mux_32to1.v
+
 `timescale 1ns/10ps
 
-module mux_32to1(
-	input wire [31:0] BusMuxIn_R0,
-	input wire [31:0] BusMuxIn_R1,
-	input wire [31:0] BusMuxIn_R2,
-	input wire [31:0] BusMuxIn_R3,
-	input wire [31:0] BusMuxIn_R4,
-	input wire [31:0] BusMuxIn_R5,
-	input wire [31:0] BusMuxIn_R6,
-	input wire [31:0] BusMuxIn_R7,
-	input wire [31:0] BusMuxIn_R8,
-	input wire [31:0] BusMuxIn_R9,
-	input wire [31:0] BusMuxIn_R10,
-	input wire [31:0] BusMuxIn_R11,
-	input wire [31:0] BusMuxIn_R12,
-	input wire [31:0] BusMuxIn_R13,
-	input wire [31:0] BusMuxIn_R14,
-	input wire [31:0] BusMuxIn_R15,
-	
-	input wire [31:0] BusMuxIn_HI,
-	input wire [31:0] BusMuxIn_LO,
-	input wire [31:0] BusMuxIn_Zhigh,
-	input wire [31:0] BusMuxIn_Zlow,
-	
-	input wire [31:0] BusMuxIn_PC,
-	input wire [31:0] BusMuxIn_MDR,	
-	input wire [31:0] BusMuxIn_InPort,	
-	
+module Multiplexer(
+	input wire [31:0] BusMuxInR0,
+	input wire [31:0] BusMuxInR1,
+	input wire [31:0] BusMuxInR2,
+	input wire [31:0] BusMuxInR3,
+	input wire [31:0] BusMuxInR4,
+	input wire [31:0] BusMuxInR5,
+	input wire [31:0] BusMuxInR6,
+	input wire [31:0] BusMuxInR7,
+	input wire [31:0] BusMuxInR8,
+	input wire [31:0] BusMuxInR9,
+	input wire [31:0] BusMuxInR10,
+	input wire [31:0] BusMuxInR11,
+	input wire [31:0] BusMuxInR12,
+	input wire [31:0] BusMuxInR13,
+	input wire [31:0] BusMuxInR14,
+	input wire [31:0] BusMuxInR15,
+	input wire [31:0] BusMuxInHI,
+	input wire [31:0] BusMuxInLO,
+	input wire [31:0] BusMuxInZhigh,
+	input wire [31:0] BusMuxInZlow,
+	input wire [31:0] BusMuxInPC,
+	input wire [31:0] BusMuxInMDR,	
+	input wire [31:0] BusMuxInInPort,	
 	input wire [31:0] C_sign_extended,
-	
 	output reg [31:0] BusMuxOut,
-	input wire [4:0] select_signal
-	);
+	input wire [4:0] select_signal);
 		
 	always@(*) begin
 		case(select_signal)
-         5'd0 : BusMuxOut <= BusMuxIn_R0[31:0];
-         5'd1 : BusMuxOut <= BusMuxIn_R1[31:0];
-         5'd2 : BusMuxOut <= BusMuxIn_R2[31:0];
-         5'd3 : BusMuxOut <= BusMuxIn_R3[31:0];
-			5'd4 : BusMuxOut <= BusMuxIn_R4[31:0];
-         5'd5 : BusMuxOut <= BusMuxIn_R5[31:0];
-         5'd6 : BusMuxOut <= BusMuxIn_R6[31:0];
-         5'd7 : BusMuxOut <= BusMuxIn_R7[31:0];
-			5'd8 : BusMuxOut <= BusMuxIn_R8[31:0];
-         5'd9 : BusMuxOut <= BusMuxIn_R9[31:0];
-         5'd10: BusMuxOut <= BusMuxIn_R10[31:0];
-         5'd11: BusMuxOut <= BusMuxIn_R11[31:0];
-			5'd12: BusMuxOut <= BusMuxIn_R12[31:0];
-         5'd13: BusMuxOut <= BusMuxIn_R13[31:0];
-         5'd14: BusMuxOut <= BusMuxIn_R14[31:0];
-         5'd15: BusMuxOut <= BusMuxIn_R15[31:0];
-			5'd16: BusMuxOut <= BusMuxIn_HI[31:0];
-         5'd17: BusMuxOut <= BusMuxIn_LO[31:0];
-         5'd18: BusMuxOut <= BusMuxIn_Zhigh[31:0];
-         5'd19: BusMuxOut <= BusMuxIn_Zlow[31:0];
-			5'd20: BusMuxOut <= BusMuxIn_PC[31:0];
-         5'd21: BusMuxOut <= BusMuxIn_MDR[31:0];
-         5'd22: BusMuxOut <= BusMuxIn_InPort[31:0];
-         5'd23: BusMuxOut <= C_sign_extended[31:0];
+         		5'd0 : BusMuxOut <= BusMuxInR0[31:0];
+         		5'd1 : BusMuxOut <= BusMuxInR1[31:0];
+         		5'd2 : BusMuxOut <= BusMuxInR2[31:0];
+         		5'd3 : BusMuxOut <= BusMuxInR3[31:0];
+			5'd4 : BusMuxOut <= BusMuxInR4[31:0];
+		 	5'd5 : BusMuxOut <= BusMuxInR5[31:0];
+         		5'd6 : BusMuxOut <= BusMuxInR6[31:0];
+         		5'd7 : BusMuxOut <= BusMuxInR7[31:0];
+			5'd8 : BusMuxOut <= BusMuxInR8[31:0];
+         		5'd9 : BusMuxOut <= BusMuxInR9[31:0];
+         		5'd10: BusMuxOut <= BusMuxInR10[31:0];
+         		5'd11: BusMuxOut <= BusMuxInR11[31:0];
+			5'd12: BusMuxOut <= BusMuxInR12[31:0];
+         		5'd13: BusMuxOut <= BusMuxInR13[31:0];
+         		5'd14: BusMuxOut <= BusMuxInR14[31:0];
+         		5'd15: BusMuxOut <= BusMuxInR15[31:0];
+			5'd16: BusMuxOut <= BusMuxInHI[31:0];
+         		5'd17: BusMuxOut <= BusMuxInLO[31:0];
+         		5'd18: BusMuxOut <= BusMuxInZhigh[31:0];
+         		5'd19: BusMuxOut <= BusMuxInZlow[31:0];
+			5'd20: BusMuxOut <= BusMuxInPC[31:0];
+         		5'd21: BusMuxOut <= BusMuxInMDR[31:0];
+         		5'd22: BusMuxOut <= BusMuxInInPort[31:0];
+         		5'd23: BusMuxOut <= C_sign_extended[31:0];
 			default: BusMuxOut <= 32'd0;
       endcase
    end
