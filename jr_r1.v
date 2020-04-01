601a8f04fff29d8910d49ca7aab201c71258786d
16elp <44043175+16elp@users.noreply.github.com>
2020-04-01 13:49:03 -0400

    Update and rename jr_r1.txt to jr_r1.v

--- jr_r1.v
+++ jr_r1.v
@@ -1,34 +1,43 @@
 //insruction: 98800000
+
+
 Reg_load1a: begin
 	Mdatain <= 32'h0000ABCD; 
 	enableMDR <= 1; MDR_read<=3'd2;
 	#15 enableMDR <= 0;MDR_read<=3'd0;
 end
-Reg_load1b: begin    // initialize R1 with the value 0000ABCD
+
+Reg_load1b: begin    
 	MDRout <= 1; enableR<=16'h0002;   
 	#15 MDRout <= 0; enableR<= 16'd0; 
 end
-Reg_load3a: begin		//Load PC with start of program in RAM
+
+Reg_load3a: begin	
 	Mdatain <= 32'h00000000;   
 	enableMDR <= 1;MDR_read<=3'd2;
 	#15 enableMDR <= 0;MDR_read<=3'd0; 
 end
+
 Reg_load3b: begin
 	MDRout <= 1; enablePC<=1;	
 	#15 MDRout <= 0; enablePC<=0;
 end
+
 T0: begin 
 	PCout <= 1; enableMAR <= 1; 
 	#15 PCout <= 0; enableMAR <= 0; 
 end
+
 T1: begin
-	enableMDR <= 1; MDR_read<=3'd1;   //Loads MDR from RAM output
+	enableMDR <= 1; MDR_read<=3'd1; \
 	#15 enableMDR <= 0; MDR_read<=3'd0;
 end
+
 T2: begin
 	MDRout <= 1; enableIR <= 1;
 	#15 MDRout <= 0; enableIR <= 0;			
 end
+
 T3: begin
 	Gra<=1;Rout<=1;enablePC<=1;
 	#15 Gra<=1;Rout<=1;enablePC<=0;
