cfb1651e639ad24206441b6c22f98898967c80f2
16elp <44043175+16elp@users.noreply.github.com>
2020-04-05 00:14:02 -0400

    Update DIV.v

--- DIV.v
+++ DIV.v
@@ -1,35 +1,22 @@
 
-module DIVcomplex(
-		input [31:0] Q,
-		input [31:0] M,
-		input start,
-		input clk,		
-		output reg [31:0] quotient,
-		output wire [31:0] remainder,
-		output wire ready
-);
-
-   reg [63:0]   Q_copy, M_copy, diff;
-	assign remainder = Q_copy[31:0];
-
+module DIV(input [31:0] Q, input [31:0] M, input start, input clk, output reg [31:0] quotient, output wire [31:0] remainder, output wire ready);
+   reg [63:0]   Q2, M2, diff;
+	assign remainder = Q2[31:0];
    reg [5:0] bit;
    assign ready = !bit;
-
    initial bit = 0;
 
    always @( posedge clk )
-
      if ( ready && start ) begin
         bit = 32;
         quotient = 0;
-        Q_copy = {32'd0,Q};
-        M_copy = {1'b0,M,31'd0};
-
+        Q2 = {32'd0,Q};
+        M2 = {1'b0,M,31'd0};
      end else begin
-        diff = Q_copy - M_copy;
+        diff = Q2 - M2;
         quotient = { quotient[30:0], ~diff[63] };
-        M_copy = { 1'b0, M_copy[63:1] };
-        if ( !diff[63] ) Q_copy = diff;
+	M2 = { 1'b0, M2[63:1] };
+	if ( !diff[63] ) Q2 = diff;
         bit = bit - 1;
      end
 
