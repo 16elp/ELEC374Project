868bbfaeb974c369abb6dfbb045a084aa9d1b1f7
16elp <44043175+16elp@users.noreply.github.com>
2020-04-05 00:46:19 -0400

    Update OR.v

--- OR.v
+++ OR.v
@@ -1,11 +1,7 @@
 
 
 `timescale 1ns / 1ps
-module OR(
-	input wire [31:0] Ra,
-	input wire [31:0] Rb,
-	output wire [31:0] Rz
-	);
+module OR(input wire [31:0] Ra, input wire [31:0] Rb, output wire [31:0] Rz);
 	genvar i;
 	generate
 		for (i=0; i<32;i=i+1) begin : loop
