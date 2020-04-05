204df3e73e97783de147fdd0cc3f71ce0701457d
16elp <44043175+16elp@users.noreply.github.com>
2020-04-04 23:45:53 -0400

    Update RAM.v

--- RAM.v
+++ RAM.v
@@ -1,11 +1,9 @@
-
+.
 module RAM(input [31:0] data, input [7:0] address, input we, clk, output [31:0] q);
-
 	reg [31:0] ram[511:0];
 	reg [31:0] addressReg;
 	
-	initial
-	begin : INIT
+	initial begin : INIT
 		$readmemh("init.mif", ram); 
 	end
 	
